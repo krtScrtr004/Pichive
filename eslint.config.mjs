36530c6cfb1502204e5807dde065c8be1aa6d27d
Kurt Secretario <secretariokurt3219@gmail.com>
2024-10-09 17:47:35 +0800

    <config>: add eslint config files

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+
+export default [
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 1,
+      'no-undef': 2,
+      'no-use-before-define': 1,
+      'no-unused-expressions': 1,
+      'no-var': 2,
+      'prefer-const': 1,
+      'no-multi-spaces': 1,
+      'no-shadow': 2,
+      'no-redeclare': 2,
+      eqeqeq: [2, 'always'],
+      quotes: [1, 'single'],
+      semi: [2, 'always'],
+      indent: [2, 2],
+      curly: 2,
+    },
+  },
+];
