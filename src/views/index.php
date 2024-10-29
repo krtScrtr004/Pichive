e02c984c0fb055664e22e44dcdcb614546a078d1
Kurt Secretario <secretariokurt3219@gmail.com>
2024-10-29 15:24:03 +0800

    <view>: add framework view for signup section

--- src/views/index.php
+++ src/views/index.php
@@ -7,7 +7,28 @@
 </head>
 <body>
 
-    
+    <main>
+        <!-- Banner Section -->
+        <section></section>
+
+        <!-- Login Section -->
+         <section></section>
+
+         <!-- Signup Section -->
+          <section id="signup">
+            <form action="">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" autocomplete="on" required>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" autocomplete="on" required>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+                <label for="c_password">Confirm Password</label>
+                <input type="c_password" name="c_password" id="c_password" required>
+                <button id="signup_btn" type="submit">SUBMIT</button>
+            </form>
+          </section>
+    </main>
     
 </body>
 </html>
