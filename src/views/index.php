89d49976c41266f7fae30ecd9928fcd173d16a41
Kurt Secretario <secretariokurt3219@gmail.com>
2024-10-30 11:43:46 +0800

    <view>: add framework view for login section

--- src/views/index.php
+++ src/views/index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
 </head>
+
+<style>
+    form>* {
+        display: block;
+    }
+</style>
+
 <body>
 
     <main>
@@ -12,19 +19,27 @@
         <section></section>
 
         <!-- Login Section -->
-         <section></section>
+         <section id="login">
+            <form id="login_form" action="" method="POST">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" autocomplete="on" required>
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password" required>
+                <button id="login_btn" type="submit">LOG IN</button>
+            </form>
+         </section>
 
          <!-- Signup Section -->
           <section id="signup">
-            <form id="signup_form" action="" method="POST>
+            <form id="signup_form" action="" method="POST">
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" autocomplete="on" required>
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" autocomplete="on" required>
                 <label for="password">Password</label>
-                <input type="text" name="password" id="password" required>
+                <input type="password" name="password" id="password" required>
                 <label for="c_password">Confirm Password</label>
-                <input type="text" name="c_password" id="c_password" required>
+                <input type="password" name="c_password" id="c_password" required>
                 <button id="signup_btn" type="submit">SUBMIT</button>
             </form>
 
