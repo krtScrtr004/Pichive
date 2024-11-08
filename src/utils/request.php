d93e3e9d0afaf871a27ab4d00da7da80de535836
Kurt Secretario <secretariokurt3219@gmail.com>
2024-11-08 22:49:03 +0800

    <func>: add file sender function using curl

--- src/utils/request.php
+++ src/utils/request.php
@@ -28,3 +28,20 @@ function send_data($url, $obj)
     return json_decode($response);
 }
 
+function send_file($url, $obj) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $obj);
+
+    // Execute the request and handle errors
+    $response = curl_exec($ch);
+    curl_close($ch);
+    if (!$response) {
+        return json_encode(array(
+            'status' => 'fail',
+            'message' => 'Data cannot be processed!'
+        ));
+    }
+    return json_decode($response);
+}
