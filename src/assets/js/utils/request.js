42d354751aef4e64d9675b7bc352dde052a4cb85
Kurt Secretario <secretariokurt3219@gmail.com>
2024-11-08 23:18:51 +0800

    <func>: add file sender function

--- src/assets/js/utils/request.js
+++ src/assets/js/utils/request.js
@@ -19,3 +19,21 @@ export async function sendData(URL, OBJ) {
 		console.error(`Network Error: ${e}`)
 	}
 }
+
+export async function send_file(URL, OBJ) {
+	try {
+		const response = await fetch(URL, {
+			method: 'POST',
+			body: OBJ,
+		})
+
+		const data = await response.json()
+		if (response.status !== 200) {
+			console.error(`Response Error: ${data.message}`)
+		}
+		return data
+	} catch (e) {
+		console.error(`Network Error: ${e}`)
+	}
+}
+
