72779d94f82ca2b0da4b9e987f0ee741f5d47bf1
Kurt Secretario <secretariokurt3219@gmail.com>
2024-11-20 21:20:07 +0800

    <script>: add logic to fetch followed users list every 30 secs

--- src/assets/js/event/fetch_sidenav_user.js
+++ src/assets/js/event/fetch_sidenav_user.js
@@ -1,40 +1,16 @@
-import { get_data, test_response } from '../utils/request.js'
+import { fetch_followed_user } from '../utils/fetch_sidenav_user.util.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
-    const sidenav_result = document.querySelector('.sidenav-result')
+	const sidenav_result = document.querySelector('.sidenav-result')
 
-    try {
-        const response  = await get_data(`../api/fetch_user.php?relation=followed`);
-        const test = test_response(response)
-        if (!test) {
-			sidenav_result.innerHTML = test['message']
-			return
-		}
-
-        const data = response['data']
-        if (data.length === 0) {
-            sidenav_result.innerHTML = 'You have no followed users.'
-            return
-        }
-        data.forEach((user) => {
-            display_sidenav_user(user)
-        })
-
-    } catch (error) {
-        sidenav_result.innerHTML = error['message']
-    }
-
-    function display_sidenav_user(user) {
-        const following_user_list = document.querySelector('#following_user_list')
-
-        const sidenav_user_HTML = `
-            <li class="link-wrapper">
-                <a class="nav-link" href="../views/profile.php?page=profile&id=${user.id ?? null}">
-                    <img class="circle" src="${user.profile_url ?? '../assets/img/icons/Light/Profile.svg'}" alt="" width="32" height="32">
-                    <h3>${user.username ?? 'Anonymous'}</h3>
-                </a>
-            </li>`
-
-        following_user_list.insertAdjacentHTML('beforeend', sidenav_user_HTML)
-    }
+	try {
+        // Fetch followed users for the first time
+		fetch_followed_user()
+        // Fetch followed users every 30 seconds
+		setTimeout(() => {
+			fetch_followed_user()
+		}, 30000)
+	} catch (error) {
+		sidenav_result.innerHTML = error['message']
+	}
 })
---
+++ src/assets/js/utils/fetch_sidenav_user.util.js
@@ -0,0 +1,41 @@
+import { get_data, test_response } from '../utils/request.js'
+
+function display_sidenav_user(user) {
+    const following_user_list = document.querySelector('#following_user_list')
+
+    const sidenav_user_HTML = `
+        <li class="link-wrapper">
+            <a class="nav-link" href="../views/profile.php?page=profile&id=${user.id ?? null}">
+                <img class="circle" src="${user.profile_url ?? '../assets/img/icons/Light/Profile.svg'}" alt="" width="32" height="32">
+                <h3>${user.username ?? 'Anonymous'}</h3>
+            </a>
+        </li>`
+
+    following_user_list.insertAdjacentHTML('beforeend', sidenav_user_HTML)
+}
+
+export async function fetch_followed_user() {
+    try {
+        const response  = await get_data(`../api/fetch_user.php?relation=followed`);
+        const test = test_response(response)
+        if (!test) {
+			throw new Error(test['message'])
+		}
+
+        const data = response['data']
+        if (data.length === 0) {
+            document.querySelector('.sidenav-result').innerHTML = 'You have no followed users.'
+            return
+        }
+        data.forEach((user, index) => {
+            if (index >= 5) {
+                return
+            }
+
+            display_sidenav_user(user)
+        })
+
+    } catch (error) {
+        sidenav_result.innerHTML = error['message']
+    }
+}
