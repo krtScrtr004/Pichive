e50b9a8d18b241bfb406067ef3781e649d94033d
Kurt Secretario <secretariokurt3219@gmail.com>
2024-11-20 19:08:51 +0800

    <change>: algorithm when fetching user posts according to the page

--- src/api/fetch_post.php
+++ src/api/fetch_post.php
@@ -4,61 +4,82 @@ require_once '../config/session.php';
 include_once '../utils/uuid.php';
 include_once '../utils/echo_result.php';
 
-
-if (!isset($_SESSION['user_id'])) {
-    header('Location: index.php');
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    echo_fail('Invalid request!');
-}
-
-$content_type = htmlspecialchars($_GET['content_type']) ?? 'home';
-$limit = 9;
-$offset = intval(htmlspecialchars($_GET['offset'])) ?? 0;
-
 try {
+    if (!isset($_SESSION['user_id'])) {
+        throw new Exception('Unauthorized user!');
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+        throw new Exception('Invalid request!');
+    }
+
+    $content_type = htmlspecialchars($_GET['content_type']) ?? 'home';
+    $limit = 9;
+    $offset = intval(htmlspecialchars($_GET['offset'])) ?? 0;
+
+
+
     $query = null;
     if ($content_type === 'home') {
-        // TODO: Ayusin mo ito putang ina ka
+        // Only include own, follower, and followed users' posts
         $query = $pdo->prepare("SELECT 
-                                p.id,
-                                p.title, 
-                                p.img_url, 
-                                p.description, 
-                                p.date_time, 
-                                p.likes,
-                                p.poster_id, 
-                                u.username 
-                            FROM 
-                                post as p 
-                            INNER JOIN 
-                                user as u 
-                            ON 
-                                p.poster_id = u.id 
-                            ORDER BY date_time DESC 
-                            LIMIT $limit OFFSET $offset");
-        $query->execute();
+                                    p.id,
+                                    p.title, 
+                                    p.img_url, 
+                                    p.description, 
+                                    p.date_time, 
+                                    p.likes,
+                                    p.poster_id, 
+                                    u.username 
+                                FROM 
+                                    post AS p
+                                INNER JOIN 
+                                    user AS u 
+                                ON 
+                                    p.poster_id = u.id 
+                                LEFT JOIN 
+                                    follow AS f
+                                ON 
+                                    p.poster_id = f.their_id AND f.my_id = :id
+                                WHERE 
+                                    f.my_id = :id OR p.poster_id = :id
+                                ORDER BY 
+                                    p.date_time DESC 
+                                LIMIT 
+                                    $limit OFFSET $offset");
+        $query->execute(array(
+            ':id' => encode_uuid($_SESSION['user_id'])
+        ));
     } else if ($content_type === 'explore') {
+        // Include all users' posts excluding blocked users
         $query = $pdo->prepare("SELECT 
-                                p.id,
-                                p.title, 
-                                p.img_url, 
-                                p.description, 
-                                p.date_time, 
-                                p.likes,
-                                p.poster_id, 
-                                u.username 
-                            FROM 
-                                post as p 
-                            INNER JOIN 
-                                user as u 
-                            ON 
-                                p.poster_id = u.id 
-                            ORDER BY date_time DESC 
-                            LIMIT $limit OFFSET $offset");
-        $query->execute();
+                                    p.id,
+                                    p.title, 
+                                    p.img_url, 
+                                    p.description, 
+                                    p.date_time, 
+                                    p.likes,
+                                    p.poster_id, 
+                                    u.username 
+                                FROM 
+                                    post AS p
+                                INNER JOIN 
+                                    user AS u 
+                                ON 
+                                    p.poster_id = u.id 
+                                LEFT JOIN 
+                                    block AS b 
+                                ON 
+                                    b.my_id = :id AND b.their_id = u.id
+                                WHERE 
+                                    b.their_id IS NULL
+                                ORDER BY 
+                                    p.date_time DESC 
+                                LIMIT 
+                                    $limit OFFSET $offset");
+        $query->execute(array(
+            ':id' => encode_uuid($_SESSION['user_id'])
+        ));
     } else if ($content_type === 'profile') {
         $query = $pdo->prepare("SELECT 
                                     p.id,
@@ -83,7 +104,7 @@ try {
             ':id' => encode_uuid(htmlspecialchars($_GET['id']) ?? $_SESSION['user_id'])
         ));
     }
-    
+
     $result = $query->fetchAll();
     foreach ($result as $key => &$value) {
         $value['poster_id'] = parse_uuid($value['poster_id']);
@@ -91,6 +112,6 @@ try {
     unset($value);
 
     echo_success('Successfully retrieved post data!', $result);
-} catch (PDOException $e) {
+} catch (Exception $e) {
     echo_fail($e->getMessage());
 }
