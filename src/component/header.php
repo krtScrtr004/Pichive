cb312162bde851d13d4e9d805ae2ce28e5f4e791
Kurt Secretario <secretariokurt3219@gmail.com>
2024-12-08 11:58:46 +0800

    <structure>: add other contents on header

--- src/component/header.php
+++ src/component/header.php
@@ -1,4 +1,23 @@
 <link rel="stylesheet" href="../assets/style/generic.css">
-<header class="header">
-    <button id="create_post_btn" popovertarget="create_post_modal">CREATE POST</button>
+<link rel="stylesheet" href="../assets/style/header.css">
+
+<header class="header flex-row">
+    <div class="search-area flex-row">
+        <form id="search_form" class="flex-row" action="" method="POST">
+            <input class="dark-text" type="text" name="search_input" id="search_input" min="1" max="300" placeholder="Search" required>
+
+            <button type="submit" id="search_btn">
+                <img src="../assets/img/icons/Dark/Search.svg" alt="Search Button" title="Search Button" width="24" height="24">
+            </button>
+        </form>
+    </div>
+
+    <div class="right-sub-menu flex-row">
+        <button id="create_post_btn" popovertarget="create_post_modal">CREATE POST</button>
+
+        <span class="profile-icon-menu flex-row">
+            <img class="circle" src="../assets/img/default_img_prev.png" alt="Profile Icon Menu" title="Profile Icon Menu" width="36" height="36">
+        </span>
+
+    </div>
 </header>
