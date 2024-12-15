ccc8d37280aa2b477cedb22eb41e5c6b2ad9686e
Kurt Secretario <secretariokurt3219@gmail.com>
2024-12-15 15:36:00 +0800

    <style>: change to proper style edit profile modal

--- src/assets/js/event/edit_profile.js
+++ src/assets/js/event/edit_profile.js
@@ -1,9 +1,11 @@
 import { send_file, test_response } from '../utils/request.js'
+import { form_reset } from '../utils/utils.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
 	const result = document.querySelector('.result')
 
 	const edit_profile_modal = document.querySelector('#edit_profile_modal')
+	const edit_profile_form = document.querySelector('#edit_profile_form')
 	const edit_profile_btn = document.querySelector('#edit_profile_btn')
 
 	const img_preview = document.querySelector('.img-preview.circle')
@@ -23,6 +25,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 	edit_profile_modal.onclick = (e) => {
 		if (e.target === edit_profile_modal) {
 			edit_profile_modal.classList.remove('show-modal')
+			form_reset(edit_profile_form)
 		}
 	}
 
@@ -40,9 +43,9 @@ document.addEventListener('DOMContentLoaded', async () => {
 	save_btn.onclick = async (e) => {
 		e.preventDefault()
 
-		const username = document.querySelector('#edit_profile_form #username')
-		const password = document.querySelector('#edit_profile_form #password')
-		const bio = document.querySelector('#edit_profile_form #bio')
+		const username = edit_profile_form.querySelector('#username')
+		const password = edit_profile_form.querySelector('#password')
+		const bio = edit_profile_form.querySelector('#bio')
 		const img = img_picker.files[0]
 
 		const form_data = new FormData()
@@ -58,11 +61,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 				throw new Error(test['message'])
 			}
 
-			img_preview.src = ''
-			username.value = ''
-			password.value = ''
-			bio.value = ''
-			result.innerHTML = ''
+			form_reset(edit_profile_form)
 			location.reload()
 		} catch (error) {
 			result.innerHTML = error
@@ -71,5 +70,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	cancel_btn.onclick = () => {
 		edit_profile_modal.classList.remove('show-modal')
+		form_reset(edit_profile_form)
 	}
 })
--- src/views/partials/edit_profile_modal.php
+++ src/views/partials/edit_profile_modal.php
@@ -13,11 +13,12 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
 
 <section class="modal-wrapper" id="edit_profile_modal">
     <span class="result"></span>
-    
+
     <section class="modal edit-profile light-background dark-text">
         <!-- Right halve side -->
         <div class="left-halve flex-column">
-            <h3>EDIT PROFILE INFORMATION</h3>
+            <h3 class="heading-title">EDIT PROFILE INFORMATION</h3>
+
             <div class="flex-row">
                 <img class="img-preview circle" src="../assets/img/default_img_prev.png" alt="Profile image" width="100" height="100">
                 <div class="img-button-wrapper">
@@ -25,7 +26,11 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
                     <input type="file" name="img_picker" id="img_picker" accept="image/*">
 
                     <!-- Button that would simulate the file picking through events -->
-                    <button id="img_picker_btn" class="dark-background light-text" type="button">Choose Image</button>
+                    <div class="flex-column">
+                        <button id="img_picker_btn" class="button dark-background light-text" type="button">Choose Image</button>
+
+                        <p class="img-reminder">Select only image files (jpg, jpeg, png)</p>
+                    </div>
                 </div>
             </div>
 
@@ -43,13 +48,13 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
                 <!-- Email -->
                 <div class="flex-column">
                     <label for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Email" disabled>
+                    <input type="email" name="email" id="email" placeholder="Email" value="<?php echo $_SESSION['user_email'] ?>" disabled>
                 </div>
 
                 <!-- Password -->
                 <div class="flex-column">
                     <label for="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" min="8" max="128">
+                    <input type="text" name="password" id="password" placeholder="Password" min="8" max="128">
                 </div>
 
                 <!-- Bio -->
@@ -59,12 +64,11 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
                 </div>
 
                 <!-- Buttons -->
-                <div class="button flex-row">
-                    <button id="cancel_btn" type="button">CANCEL</button>
-                    <button id="save_btn" type="submit">SAVE</button>
+                <div class="button-wrapper flex-row">
+                    <button id="cancel_btn" class="button" type="button">CANCEL</button>
+                    <button id="save_btn" class="button" type="submit">SAVE</button>
                 </div>
             </form>
         </div>
     </section>
 </section>
-
